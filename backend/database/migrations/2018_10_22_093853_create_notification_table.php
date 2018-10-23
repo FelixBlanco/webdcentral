72bd7da455e438d01a9a14c10a2a091adf641a24
jorge Gutierrez <jorguti58@gmail.com>
2018-10-22 21:54:20 -0300

    notifications

---
+++ backend/database/migrations/2018_10_22_093853_create_notification_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_notification', function (Blueprint $table) {
+            $table->increments('idNotification');
+            $table->string('titleNotification');
+            $table->string('descriptionNotification');
+            $table->integer('fk_idSecctionApp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_notification');
+    }
+}
