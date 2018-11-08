be0231e4543438d1ac8271e69c4d99772c16c6c1
ajpcve <alecortez240192@gmail.com>
2018-11-08 09:55:07 -0400

    si no envia variables entonces retorna todas la lista
    buscarGeneral

--- backend/app/Http/Controllers/ProductoController.php
+++ backend/app/Http/Controllers/ProductoController.php
@@ -5,21 +5,20 @@ namespace App\Http\Controllers;
 use App\Producto;
 use App\TagProduct;
 use Carbon\Carbon;
-use function GuzzleHttp\Promise\promise_for;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
-
-class ProductoController extends Controller {
+class ProductoController extends Controller
+{
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
 
-    public function buscarGeneral($search = null) {
+    public function buscarGeneral($search = null)
+    {
 
         if (! is_null($search)) {
 
@@ -47,19 +46,32 @@ class ProductoController extends Controller {
             ];
 
             return response()->json($response, 200);
+
         } else {
+
+            $mascotas = Producto::where('fk_idSatate', '=', 1)
+                ->get();
+
+            $marcas = Producto::where('fk_idSatate', '=', 1)
+                ->get();
+
+            $nombre = Producto::where('fk_idSatate', '=', 1)
+                ->get();
+
             $response = [
-                'msj' => 'Debe introducir un término de búsqueda',
+                'msj'      => 'Lista de producto',
+                'mascotas' => $mascotas,
+                'marcas'   => $marcas,
+                'nombre'   => $nombre,
             ];
 
             return response()->json($response, 404);
-
         }
     }
 
-
-    public function listarPorIsOutstanding() {
-        $producto_activador    = Producto::where('isOutstanding', 1)->get();
+    public function listarPorIsOutstanding()
+    {
+        $producto_activador = Producto::where('isOutstanding', 1)->get();
         $producto_desactivador = Producto::where('isOutstanding', 0)->get();
 
         $response = [
@@ -71,18 +83,20 @@ class ProductoController extends Controller {
         return response()->json($response, 200);
     }
 
-    public function onIsOutstanding($idProducto) {
+    public function onIsOutstanding($idProducto)
+    {
         DB::beginTransaction();
 
         try {
             $producto = Producto::findOrFail($idProducto);
-            $producto->fill([ 'isOutstanding'      => 1,
-                              'fechaIsOutstanding' => Carbon::now()->toDateTimeString() ]);
+            $producto->fill([
+                'isOutstanding'      => 1,
+                'fechaIsOutstanding' => Carbon::now()->toDateTimeString(),
+            ]);
 
             $producto->save();
 
             DB::commit();
-
 
             $response = [
                 'msj'  => 'Producto activado',
@@ -98,18 +112,19 @@ class ProductoController extends Controller {
                 'message' => 'Ha ocurrido un error al tratar de guardar los datos.',
             ], 500);
         }
-
-
     }
 
-    public function offIsOutstanding($idProducto) {
+    public function offIsOutstanding($idProducto)
+    {
 
         DB::beginTransaction();
 
         try {
             $producto = Producto::findOrFail($idProducto);
-            $producto->fill([ 'isOutstanding'      => 0,
-                              'fechaIsOutstanding' => Carbon::now()->toDateTimeString() ]);
+            $producto->fill([
+                'isOutstanding'      => 0,
+                'fechaIsOutstanding' => Carbon::now()->toDateTimeString(),
+            ]);
             $producto->save();
 
             DB::commit();
@@ -128,11 +143,10 @@ class ProductoController extends Controller {
                 'message' => 'Ha ocurrido un error al tratar de guardar los datos.',
             ], 500);
         }
-
-
     }
 
-    public function listar(Request $request) {
+    public function listar(Request $request)
+    {
 
         if ($request->exists('offset') && $request->exists('limit')) {
 
@@ -147,21 +161,13 @@ class ProductoController extends Controller {
                 'limit.min'  => 'Debe tener al menos un número',
             ]);
 
-            $productos = Producto::offset($request->offset)
-                ->limit($request->limit)
-                ->get();
-
+            $productos = Producto::offset($request->offset)->limit($request->limit)->get();
         } else {
             if ($request->exists('search')) {
 
                 $busqueda = "%".$request->search."%";
 
-                $productos = Producto::where('nombre', 'like', $busqueda)
-                    ->where('fk_idSatate', '=', 1)
-                    ->orWhere('titulo', 'like', $busqueda)
-                    ->orWhere('categoria', 'like', $busqueda)
-                    ->get();
-
+                $productos = Producto::where('nombre', 'like', $busqueda)->where('fk_idSatate', '=', 1)->orWhere('titulo', 'like', $busqueda)->orWhere('categoria', 'like', $busqueda)->get();
             } else {
 
                 $productos = Producto::where('fk_idSatate', '=', 1)->get();
@@ -176,12 +182,12 @@ class ProductoController extends Controller {
         return response()->json($response, 201);
     }
 
-    public function listarPorNombre($nombre) {
+    public function listarPorNombre($nombre)
+    {
 
         $busqueda = $nombre."%";
 
-        $productos = Producto::where('nombre', 'like', $busqueda)
-            ->where('fk_idSatate', '=', 1)->get();
+        $productos = Producto::where('nombre', 'like', $busqueda)->where('fk_idSatate', '=', 1)->get();
 
         $response = [
             'msj'       => 'Lista de productos',
@@ -191,117 +197,114 @@ class ProductoController extends Controller {
         return response()->json($response, 201);
     }
 
-
-    public function index() {
+    public function index()
+    {
         // todos los productos 
         return Producto::get();
     }
 
-
-    public static function create($_product) {
+    public static function create($_product)
+    {
         $rs = Producto::where("codeProdSys", "=", $_product['codeProdSys'])->first();
 
         if (! $rs) {
 
-            $product                = new Producto();
-            $product->nombre        = $_product['nombre'];
-            $product->titulo        = $_product['titulo'];
-            $product->urlImage      = $_product['urlImage'];
-            $product->promocion     = $_product['promocion'];
-            $product->codeProdSys   = $_product['codeProdSys'];
-            $product->kiloProdcuto  = $_product['kiloProdcuto'];
-            $product->SubRubro1     = $_product['SubRubro1'];
-            $product->SubRubro2     = $_product['SubRubro2'];
-            $product->precioL1      = $_product['precioL1'];
-            $product->precioL2      = $_product['precioL2'];
-            $product->precioL3      = $_product['precioL3'];
-            $product->precioL4      = $_product['precioL4'];
-            $product->precioL5      = $_product['precioL5'];
-            $product->precioL6      = $_product['precioL6'];
-            $product->precioL7      = $_product['precioL7'];
-            $product->precioL8      = $_product['precioL8'];
-            $product->precioL9      = $_product['precioL9'];
-            $product->rubro         = $_product['rubro'];
-            $product->marca         = $_product['marca'];
-            $product->fk_idSatate   = 1;
-            $product->destacado     = 0;
+            $product = new Producto();
+            $product->nombre = $_product['nombre'];
+            $product->titulo = $_product['titulo'];
+            $product->urlImage = $_product['urlImage'];
+            $product->promocion = $_product['promocion'];
+            $product->codeProdSys = $_product['codeProdSys'];
+            $product->kiloProdcuto = $_product['kiloProdcuto'];
+            $product->SubRubro1 = $_product['SubRubro1'];
+            $product->SubRubro2 = $_product['SubRubro2'];
+            $product->precioL1 = $_product['precioL1'];
+            $product->precioL2 = $_product['precioL2'];
+            $product->precioL3 = $_product['precioL3'];
+            $product->precioL4 = $_product['precioL4'];
+            $product->precioL5 = $_product['precioL5'];
+            $product->precioL6 = $_product['precioL6'];
+            $product->precioL7 = $_product['precioL7'];
+            $product->precioL8 = $_product['precioL8'];
+            $product->precioL9 = $_product['precioL9'];
+            $product->rubro = $_product['rubro'];
+            $product->marca = $_product['marca'];
+            $product->fk_idSatate = 1;
+            $product->destacado = 0;
             $product->isOutstanding = 0;
 
             $product->save();
         } else {
-            $rs->nombre       = $_product['nombre'];
-            $rs->titulo       = $_product['titulo'];
-            $rs->urlImage     = $_product['urlImage'];
-            $rs->promocion    = $_product['promocion'];
-            $rs->codeProdSys  = $_product['codeProdSys'];
+            $rs->nombre = $_product['nombre'];
+            $rs->titulo = $_product['titulo'];
+            $rs->urlImage = $_product['urlImage'];
+            $rs->promocion = $_product['promocion'];
+            $rs->codeProdSys = $_product['codeProdSys'];
             $rs->kiloProdcuto = $_product['kiloProdcuto'];
-            $rs->SubRubro1    = $_product['SubRubro1'];
-            $rs->SubRubro2    = $_product['SubRubro2'];
-            $rs->precioL1     = $_product['precioL1'];
-            $rs->precioL2     = $_product['precioL2'];
-            $rs->precioL3     = $_product['precioL3'];
-            $rs->precioL4     = $_product['precioL4'];
-            $rs->precioL5     = $_product['precioL5'];
-            $rs->precioL6     = $_product['precioL6'];
-            $rs->precioL7     = $_product['precioL7'];
-            $rs->precioL8     = $_product['precioL8'];
-            $rs->precioL9     = $_product['precioL9'];
-            $rs->rubro        = $_product['rubro'];
-            $rs->marca        = $_product['marca'];
-            $rs->fk_idSatate  = 1;
+            $rs->SubRubro1 = $_product['SubRubro1'];
+            $rs->SubRubro2 = $_product['SubRubro2'];
+            $rs->precioL1 = $_product['precioL1'];
+            $rs->precioL2 = $_product['precioL2'];
+            $rs->precioL3 = $_product['precioL3'];
+            $rs->precioL4 = $_product['precioL4'];
+            $rs->precioL5 = $_product['precioL5'];
+            $rs->precioL6 = $_product['precioL6'];
+            $rs->precioL7 = $_product['precioL7'];
+            $rs->precioL8 = $_product['precioL8'];
+            $rs->precioL9 = $_product['precioL9'];
+            $rs->rubro = $_product['rubro'];
+            $rs->marca = $_product['marca'];
+            $rs->fk_idSatate = 1;
             $rs->update();
-
         }
     }
 
+    public static function createTag($_tag)
+    {
 
-    public static function createTag($_tag) {
-
-        $rs = TagProduct::where("codeProdSys", "=", $_tag['codeProdSys'])
-            ->where("tag", "=", $_tag['tag'])
-            ->first();
+        $rs = TagProduct::where("codeProdSys", "=", $_tag['codeProdSys'])->where("tag", "=", $_tag['tag'])->first();
 
         if (! is_null($_tag['tag']) && ! is_null($_tag['codeProdSys'])) {
             if (! $rs) {
-                $tag              = new TagProduct();
-                $tag->tag         = @$_tag['tag'];
+                $tag = new TagProduct();
+                $tag->tag = @$_tag['tag'];
                 $tag->codeProdSys = @$_tag['codeProdSys'];
                 $tag->fk_idSatate = 1;
                 $tag->save();
             } else {
-                $tag              = new TagProduct();
+                $tag = new TagProduct();
                 $tag->fk_idSatate = 1;
                 $tag->update();
-
             }
         }
     }
 
-
-    public static function getAllTags() {
+    public static function getAllTags()
+    {
 
         $response = TagProduct::select("tag")->distinct('tag')->orderBy("tag")->get();
 
         return response()->json($response, 202);
     }
 
-
-    public static function getAllRubros() {
-
+    public static function getAllRubros()
+    {
 
         $response = Producto::select("rubro")->distinct('rubro')->orderBy("rubro")->get();
 
         return response()->json($response, 202);
     }
 
-    public static function getAllMarcas() {
+    public static function getAllMarcas()
+    {
 
         $response = Producto::select("marca")->distinct('marca')->orderBy("marca")->get();
 
         return response()->json($response, 202);
     }
 
-    public static function searchMarca($search = null) {
+    public static function searchMarca($search = null)
+    {
 
         if (! is_null($search)) {
             $busqueda = $search."%";
@@ -316,7 +319,6 @@ class ProductoController extends Controller {
             } else {
                 return response()->json($response, 202);
             }
-
         } else {
 
             $response = [
@@ -324,9 +326,6 @@ class ProductoController extends Controller {
             ];
 
             return response()->json($response, 404);
-
         }
     }
-
-
 }
