6089e09960fbfddbdf5b586564fcce93e1de7e86
Armando Perdomo <armandoperdomoing@gmail.com>
2018-11-22 14:30:50 -0400

    gitignore environments

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
-export const ip = 'http://127.0.0.1:8888'
+export const ip = 'http://127.0.0.1:8000'
 
 export const environment = {
   production: true,
-  apiHost:  `${ip}/webdcentral/backend/public`
+  apiHost:  `${ip}`
 };
