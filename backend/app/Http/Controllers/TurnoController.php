ce9fc5d30e4fed64e746d67571135df3c3ba1e34
Alejandro Parrilla <alecortez240192@gmail.com>
2018-12-03 09:55:17 -0400

    comentado el envio de correo en turno controller

--- backend/app/Http/Controllers/TurnoController.php
+++ backend/app/Http/Controllers/TurnoController.php
@@ -51,7 +51,7 @@ class TurnoController extends Controller {
             ];
 
 
-            Mail::to(Auth::user()->email)->send(new TurnosMail($turno));
+            //Mail::to(Auth::user()->email)->send(new TurnosMail($turno));
             DB::commit();
 
             return response()->json($response, 201);
