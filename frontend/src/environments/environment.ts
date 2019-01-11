0c3f68c1e971a461d7eed6a4151c820ccce00b66
erick gonzalez <erickgonzalezdaniel@gmail.com>
2019-01-10 20:12:55 -0430

    ajustes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const ip = 'http://127.0.0.1:8000'
-
+//http://depocentral.dyndns.org:8753    api servidor
+//http://127.0.0.1:8000   api local
 export const environment = {
   production: false,
   apiHost:  `${ip}`
