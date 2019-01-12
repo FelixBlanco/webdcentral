e80ff4a5def3cbb539628e9e8449d3930a576626
jorge Gutierrez <jorguti58@gmail.com>
2019-01-12 13:32:19 -0300

    version server

--- registroRuta/src/environments/environment.ts
+++ registroRuta/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const ip = 'http://127.0.0.1:8000'
+export const ip = 'http://depocentral.dyndns.org:8753'
 
 export const environment = {
   production: false,
