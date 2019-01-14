d831b83f9a987b79007a958f0c05513fc826bf65
danielhumgon <danielhumgon@gmail.com>
2019-01-14 22:25:25 +0200

    new changes 2

--- frontend/src/app/component/entrega/entrega.component.ts
+++ frontend/src/app/component/entrega/entrega.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfgFooterService } from '../../services/confg-footer.service';
 
 @Component({
   selector: 'app-entrega',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntregaComponent implements OnInit {
 
-  constructor() { }
+  data:any = { 
+    img_envio_1: null, 
+    img_envio_2: null, 
+    img_envio_3: null, 
+    
+  };
 
-  ngOnInit() {
+  constructor(
+    private _confgFooterService:ConfgFooterService,
+  ) { }
+
+  ngOnInit() { 
+    this.serviceCall();
+  }
+
+  serviceCall() {
+    this._confgFooterService._getConfigFooter().subscribe(
+      (resp: any) => {
+        if (resp) {
+          this.data.img_envio_1 = resp.img_envio_1;
+          this.data.img_envio_2 = resp.img_envio_2;
+          this.data.img_envio_3 = resp.img_envio_3;
+          console.log(this.data);
+        }
+      }
+    ) 
   }
 
 }
