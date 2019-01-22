0173181bfbd64cbf3ebf0d1619be7f8e9e375371
Alejandro Parrilla <alecortez240192@gmail.com>
2019-01-22 16:44:43 -0400

    add nuevos campos en domicilios

--- backend/app/Domicilio.php
+++ backend/app/Domicilio.php
@@ -5,18 +5,23 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Domicilio extends Model {
-
+class Domicilio extends Model
+{
     use SoftDeletes;
-    protected $table      = 'tb_domicilios';
+
+    protected $table = 'tb_domicilios';
+
     protected $primaryKey = 'idDomicilios';
 
     protected $fillable = [
         'descripcion',
         'fk_idCliente',
+        'provincia',
+        'localidad',
     ];
 
-    public function Cliente() {
+    public function Cliente()
+    {
         return $this->belongsTo('App\User', 'fk_idCliente');
     }
 }
