2db0e627983b55953b804eda4a768abdd1205a56
Alejandro Parrilla <alecortez240192@gmail.com>
2019-01-25 11:36:12 -0400

    add reservaMercaderiaHrs en config footer

--- backend/app/Http/Controllers/ConfigFooterController.php
+++ backend/app/Http/Controllers/ConfigFooterController.php
@@ -196,6 +196,7 @@ class ConfigFooterController extends Controller
             $d->img_envio_2 = $request->img_envio_2;
             $d->img_envio_3 = $request->img_envio_3;
             $d->img_como_comprar = $request->img_como_comprar;
+            $d->reservaMercaderiaHrs = $request->reservaMercaderiaHrs;
 
             if (is_null($request->imagen)) {
             } else {
