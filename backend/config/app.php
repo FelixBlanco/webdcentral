82f6937b9ba6c7a05d7bfa5b038bd60eb6f22cf2
jorge Gutierrez <jorguti58@gmail.com>
2019-01-30 17:49:48 -0300

    s

--- backend/config/app.php
+++ backend/config/app.php
@@ -174,6 +174,7 @@ return [
         Barryvdh\Cors\ServiceProvider::class,
         //Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
+        Barryvdh\DomPDF\ServiceProvider::class,
     ],
 
     /*
@@ -228,7 +229,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
 
-
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
